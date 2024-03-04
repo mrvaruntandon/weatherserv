4ae6e784ce6124d5d2066e30266029a244df667f
Varun Tandon <mrvaruntandon@gmail.com>
2024-03-04 14:03:20 +0530

    feat: initial commit

---
+++ src/main/java/com/github/mrvaruntandon/weatherserv/WeatherservApplication.java
@@ -0,0 +1,13 @@
+package com.github.mrvaruntandon.weatherserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mrvaruntandon/weatherserv/WeatherservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mrvaruntandon.weatherserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
